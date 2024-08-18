0ae9cf242ec2b91f1a4b10f022aaeef573937abb
Wesley Serrano <wesley.serrano@cwi.com.br>
2024-08-18 02:21:21 -0300

    Adicionando instruções ao script; - Adicionando endpoint à aplicação Go;

--- imagens/build-container.sh
+++ imagens/build-container.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+docker stop docker-go-local
+docker rm docker-go-local
+docker image rm docker-go-local:0.0.1
 docker build --label docker-go -t docker-go:0.0.1  -f dockerfile-go .
-docker run -d -l docker-go-local --name docker-go-local -p 8181:8181 docker-go
+docker run -d -l docker-go-local --name docker-go-local -p 8181:8181 docker-go:0.0.1
